102d5cf6136e56cce4c5021058cbf0a6e27f454f
Nicholas Dry <nicholasdry@me.com>
2016-02-21 18:55:08 -0500

    Update HTMLGeneratorGUI.java

--- src/htmlgenerator/HTMLGeneratorGUI.java
+++ src/htmlgenerator/HTMLGeneratorGUI.java
@@ -1,7 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Nicholas Dry
+ * Copyright 2016
  */
 package htmlgenerator;
 
